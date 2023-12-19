0d04b8f640f9cc5f4579592409d6b4cfad64d571
blackmonkDMO <blackmonk_dmo@yahoo.com>
2023-12-19 20:41:58 +0200

    index.php prepared for actual php code

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+
+session_start();
+
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -11,12 +17,32 @@
 		<nav class="navtop">
 			<div>
 				<h1>Biblioteca BMK</h1>
-				<a href="autentificare.php"><i class="fa-solid fa-right-to-bracket"></i>Autentificare</a>
+                <?php
+                if ($_SESSION['loggedin'] == True) {
+                    echo '<a href="index.php"><i class="fa-solid fa-house"></i>Acasă</a>';
+                    echo '<a href="profil.php"><i class="fas fa-user-circle"></i>Profil</a>';
+                    if ($_SESSION['rol'] == 'admin') {
+                        echo '<a href="administrare.php"><i class="fa-solid fa-hammer"></i>Administrare</a>';
+                    }
+                    echo '<a href="iesire.php"><i class="fa-solid fas fa-sign-out-alt"></i>Ieșire</a>';
+                }
+                else {
+                    echo '<a href="index.php"><i class="fa-solid fa-house"></i>Acasă</a>';
+                    echo '<a href="autentificare.php"><i class="fa-solid fa-lock"></i>Autentificare</a>';
+                }
+                ?>
 			</div>
 		</nav>
 		<div class="content">
-			<h2>Acasa</h2>
-			<p>Bine ați venit!</p>
+            <?php
+            if ($_SESSION['loggedin'] == True) {
+                echo 'Bine ai revenit, !';
+            }
+            else {
+                echo '<h2>Motto</h2>';
+                echo '<p>"Aceasta nu va fi niciodată o țară civilizată atâta timp cât cheltuim mai mulți bani pe guma de mestecat decât pe cărți."<br>Elbert Hubbard</p>';
+            }
+            ?>
 		</div>
 	</body>
 </html>
