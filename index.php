51567469b2f41dca15976ec61167c84e03717d5a
blackmonkDMO <blackmonk_dmo@yahoo.com>
2023-12-19 19:44:15 +0200

    Initial files, simple index.php (simple HTML for now) and style.css (some css fished from the big net).

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Biblioteca BMK</title>
+		<script src="https://kit.fontawesome.com/e9d05e6757.js" crossorigin="anonymous"></script>
+        <link href="style.css" rel="stylesheet" type="text/css">
+	</head>
+
+	<body class="loggedin">
+		<nav class="navtop">
+			<div>
+				<h1>Biblioteca BMK</h1>
+				<a href="autentificare.php"><i class="fa-solid fa-right-to-bracket"></i>Autentificare</a>
+			</div>
+		</nav>
+		<div class="content">
+			<h2>Acasa</h2>
+			<p>Bine ați venit!</p>
+		</div>
+	</body>
+</html>
