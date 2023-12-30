9161c92cc14c2154c88123353228604c44359681
blackmonkDMO <blackmonk_dmo@yahoo.com>
2023-12-30 19:56:12 +0200

    adding header and navtop

---
+++ header.php
@@ -0,0 +1,10 @@
+<?php
+echo '
+<head>
+	<meta charset="utf-8">
+	<title>Biblioteca BMK</title>
+	<script src="https://kit.fontawesome.com/e9d05e6757.js" crossorigin="anonymous"></script>
+        <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+';
+?>
---
+++ navtop.php
@@ -0,0 +1,26 @@
+<?php
+
+echo '
+<nav class="navtop">
+	<div>
+		<h1>Biblioteca BMK</h1>
+';
+			if ($_SESSION['loggedin'] == True && $_SESSION['cod_activare'] == 'activat') {
+				echo '<a href="index.php"><i class="fa-solid fa-house"></i>Acasă</a>';
+				echo '<a href="profil.php"><i class="fas fa-user-circle"></i>Profil</a>';
+                    		if ($_SESSION['rol'] == 'admin') {
+                        		echo '<a href="administrare.php"><i class="fa-solid fa-hammer"></i>Administrare</a>';
+                    		}
+                    		echo '<a href="iesire.php"><i class="fa-solid fas fa-sign-out-alt"></i>Ieșire</a>';
+                	}
+                	else {
+                    		echo '<a href="index.php"><i class="fa-solid fa-house"></i>Acasă</a>';
+                    		echo '<a href="inregistrare.php"><i class="fa-solid fa-user-plus"></i></i>Înregistrare</a>';
+                    		echo '<a href="autentificare.php"><i class="fa-solid fa-lock"></i>Autentificare</a>';
+                	}
+echo '
+	</div>
+</nav>
+';
+
+?>
