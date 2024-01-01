ed06d4e6ca6bfae0e69c449551cf295b6cc7b845
blackmonkDMO <blackmonk_dmo@yahoo.com>
2024-01-02 01:21:02 +0200

    adding profil (account) edit capabilties

---
+++ modificaprofil.php
@@ -0,0 +1,108 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html>
+    <?php include_once ('header.php'); ?>
+    <body class="loggedin">
+        <?php include_once ('navtop.php'); ?>
+        <div class="content">
+            <h2>Modifică profil</h2>
+            <div>
+            <?php
+            if ($_SESSION['loggedin'] == True) {                // Dacă suntem autentificați
+                if ($_SESSION['cod_activare'] == 'activat') {   // și dacă userul este activ
+                    
+                    if (!isset($_POST['nume'], $_POST['prenume'], $_POST['email']) || (empty($_POST['nume']) && empty($_POST['prenume']) && empty($_POST['email']))) {  // dacă am primit un formular gol ?>
+                        <p>Nu ați solicit modificarea niciunui detaliu al profilului dumneavoastră!</p>
+                        <p>Profilul poate fi administrat <a href=profil.php>aici</a></p>
+                    <?php }
+                    else {
+                        include_once('db.php');
+                        if ($stmt = $db->prepare('SELECT Nume, Prenume, Email FROM Conturi WHERE Id = ?')) {
+                            $stmt->bind_param('i', $_SESSION['id']);
+                            $stmt->execute();
+                            $stmt->bind_result($nume_actual, $prenume_actual, $email_actual);
+                            $stmt->fetch();
+                            $stmt->close();
+                            
+                            if (!empty($_POST['nume'])) { // Dacă s-a încercat schimbarea numelui
+                                if ($_POST['nume'] != $nume_actual) {
+                                    if ($stmt = $db->prepare('UPDATE Conturi SET Nume = ? WHERE Id = ?')) {
+                                        $stmt->bind_param('si', $_POST['nume'], $_SESSION['id']);
+                                        $stmt->execute(); ?>
+                                        <p>Am modificat nume <?php echo $nume_actual; ?> -> <?php echo $_POST['nume'] ?></p>
+                                        <?php $stmt->close;
+                                    }
+                                    else { ?>
+                                        <p>Eroare SQL!</p>
+                                    <?php }
+                                }
+                                else { ?>
+                                    <p>Numele vechi și numele nou sunt identice. Nu am operat modificarea numelui!</p>
+                                <?php }
+                            }
+                            
+                            if (!empty($_POST['prenume'])) { // Dacă s-a încercat schimbarea prenumelui
+                                if ($_POST['prenume'] != $prenume_actual) {
+                                    if ($stmt = $db->prepare('UPDATE Conturi SET Prenume = ? WHERE Id = ?')) {
+                                        $stmt->bind_param('si', $_POST['prenume'], $_SESSION['id']);
+                                        $stmt->execute(); ?>
+                                        <p>Am modificat prenume <?php echo $prenume_actual; ?> -> <?php echo $_POST['prenume'] ?></p>
+                                        <?php $stmt->close;
+                                    }
+                                    else { ?>
+                                        <p>Eroare SQL!</p>
+                                    <?php }
+                                }
+                                else { ?>
+                                    <p>Prenumele vechi și prenumele nou sunt identice. Nu am operat modificarea prenumelui!</p>
+                                <?php }
+                            }
+                            
+                            if (!empty($_POST['email'])) { // Dacă s-a încercat schimbarea emailului
+                                if ($_POST['email'] != $email_actual) {
+                                    if ($stmt = $db->prepare('UPDATE Conturi SET Email = ?, CodActivare = ? WHERE Id = ?')) {
+                                        $uniqid = uniqid();
+                                        $stmt->bind_param('ssi', $_POST['email'], $uniqid, $_SESSION['id']);
+                                        $stmt->execute();
+                                        
+                                        session_destroy();
+                                        
+                                        $from = 'noreply@blackmonk.ro';
+                                        $subject = 'daw.blackmonk.ro - Este necesara (re)activarea contului';
+                                        $headers = 'From: ' . $from . "\r\n" . 'X-Mailer: PHP/' . phpversion() . "\r\n" . 'MIME-Version: 1.0' . "\r\n" . 'Content-Type: text/html; charset=UTF-8';
+                                        $link_activare = 'https://daw.blackmonk.ro/activare.php?email=' . $_POST['email'] . '&cod=' . $uniqid;
+                                        $message = '<p>Ați solicitat schimbarea adresei de email asociată profilului dumneavoastră. Pentru a confirma adresa de email și a reactiva profilul vă rugăm să accesați acest link: <a href="' . $link_activare . '">' . $link_activare . '</a></p>';
+            
+                                        mail($_POST['email'], $subject, $message, $headers); ?>
+                
+                                        <p>Am modificat email <?php echo $email_actual; ?> -> <?php echo $_POST['email'] ?>.<br>Profilul trebuie acum reactivat folosind noua adresă de email.<br>Pentru asta a fost trimis un email la <?php echo $_POST['email'] ?> în care veți regăsi link-ul necesar.</p>
+                                        <?php $stmt->close;
+                                    }
+                                    else { ?>
+                                        <p>Eroare SQL!</p>
+                                    <?php }
+                                }
+                                else { ?>
+                                    <p>Email-ul vechi și email-ul nou sunt identice. Nu am operat modificarea email-ului!</p>
+                                <?php }
+                            }
+                            
+                        }
+                        else { ?>
+                            <p>Eroare SQL!!</p>
+                        <?php }
+                        $db->close;
+                    }
+                }
+                else { // Dacă suntem autentificați dar userul nu este activ ?>
+                    <p>Salut! Contul <strong><?php echo $_SESSION['name']; ?></strong> nu a fost activat. Vă rugăm să verificați adresa de email pentru link-ul necesar activării.<br>Puteți face logout <a href=iesire.php>aici</a>.<br>Alternativ, puteți șterge acest cont <a href=stergere.php>aici</a>.</p>
+                <?php }
+            }
+            else { // Dacă nu suntem autentificați ?>
+                <p>Pentru a accesa această secțiune este nevoie să vă autentificați <a href="autentificare.php">aici</a>.</p>
+            <?php } ?>
+            </div>
+        </div>
+    </body>
+</html>
--- profil.php
+++ profil.php
@@ -25,8 +25,12 @@ $db->close;
                 if ($_SESSION['cod_activare'] == 'activat') { ?>
                     <h2>Profil</h2>
                     <div>
-                        <p>Detaliile contului tău sunt:</p>
+                        <p><strong>Detaliile profilului tău:</strong></p>
                         <table>
+                        <tr>
+                            <td>Nume utilizator:</td>
+                            <td><?php echo $_SESSION['name']; ?></td>
+                        </tr>
                         <tr>
                             <td>Nume:</td>
                             <td><?php echo $nume; ?></td>
@@ -36,27 +40,65 @@ $db->close;
                             <td><?php echo $prenume; ?></td>
                         </tr>
                         <tr>
-                            <td>Nume utilizator:</td>
-                            <td><?php echo $_SESSION['name']; ?></td>
-                        </tr>
-                        <tr>
                             <td>Email:</td>
                             <td><?php echo $email; ?></td>
                         </tr>
                         </table>
                     </div>
+            
+                    <div>
+                        <p><strong>Modifică profilul <?php echo $_SESSION['name']; ?>:</strong></p>
+                        <form action="modificaprofil.php" method="post">
+                            <fieldset>
+                                <label for="nume">Nume:</label>
+                                <input type="text" name="nume" placeholder="<?php echo $nume; ?>" id="nume">
+                                <label for="prenume">Prenume:</label>
+                                <input type="text" name="prenume" placeholder="<?php echo $prenume; ?>" id="prenume">
+                                <label for="email">Email:</label>
+                                <input type="email" name="email" placeholder="<?php echo $email; ?>" id="email">
+                                <input type="submit" value="Modifică profil">
+                            </fieldset>
+                        </form>
+                        <p>Poți modifica numele, prenumele și adresa de email asociate profilului.</p>
+                        <p>În momentul modificării adresei de email, va fi nevoie să reconfirmați profilul pe adresa nouă.</p>
+                        <p>Nu se poate modifica numele de utilizator (nickname-ul).</p>
+                    </div>
+            
+                    <div>
+                        <p><strong>Schimbă parola profilului <?php echo $_SESSION['name']; ?>:</strong></p>
+                        <form action="schimbaparola.php" method="post">
+                            <fieldset>
+                                <label for="parolaveche">Parolă actuală:</label>
+                                <input type="password" name="parolaveche" placeholder="Introdu parola actuală a profilului" id="parolaveche" required>
+                                <label for="parolanoua">Parolă nouă:</label>
+                                <input type="password" name="parolanoua" placeholder="Introdu parola pe care vrei să o setezi profilului" id="parolanoua" required>
+                                <input type="submit" value="Schimbă parola">
+                            </fieldset>
+                        </form>
+                    </div>
+            
+                    <div>
+                        <p><strong>Șterge profil <?php echo $_SESSION['name']; ?>:</strong></p>
+                        <form action="stergeprofil.php" method="post">
+                            <fieldset>
+                                <p>Prin bifarea check-boxului de mai jos confirmați că sunteți de acrod cu ștergerea profilului. Datele profilului nu mai pot fi recuperate după ce este șters!</p>
+                                <input type="checkbox" id="confirmastergere" name="confirmastergere" value="confirmastergere">
+                                <label for="checkstergere">Sunt sigur</label>
+                                <input type="submit" value="Șterge profil">
+                            </fieldset>
+                        </form>
+                    </div>
                 <?php
                 }
-                else {
-                    echo '<h2>Cont neactivat!</h2>';
-                    echo '<p>Salut! Contul ' . $_SESSION['name'] . ' nu a fost activat. Vă rugăm să verificați adresa de email folosită la înregistrare pentru link-ul necesar activării.<br>Puteți face logout <a href=iesire.php>aici</a>.<br>Alternativ, puteți șterge acest cont <a href=stergere.php>aici</a>.</p>';
-                }
+                else { ?>
+                    <h2>Cont neactivat!</h2>
+                    <p>Salut! Contul <strong><?php echo $_SESSION['name']; ?></strong> nu a fost activat. Vă rugăm să verificați adresa de email folosită la înregistrare pentru link-ul necesar activării.<br>Puteți face logout <a href=iesire.php>aici</a>.<br>Alternativ, puteți șterge acest profil <a href=stergere.php>aici</a>.</p>
+                <?php }
             }
-            else {
-                echo '<h2>Profil - autentificare necesară</h2>';
-                echo '<p>Pentru a accesa această secțiune este nevoie să vă autentificați <a href="autentificare.php">aici</a>.</p>';
-            }
-            ?>
+            else { ?>
+                <h2>Profil - autentificare necesară</h2>
+                <p>Pentru a accesa această secțiune este nevoie să vă autentificați <a href="autentificare.php">aici</a>.</p>
+            <?php } ?>
         </div>
     </body>
 </html>
